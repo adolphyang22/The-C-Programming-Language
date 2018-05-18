768880d32de66f8ee31607cdda7633ffb9b0991c
adolphyang22 <2388965896@qq.com>
2018-05-19 02:42:26 +0800

    Create getfloat.cpp

---
+++ 5.2ec/getfloat.cpp
@@ -0,0 +1,33 @@
+#include "test1.h"
+
+/* getfloat: get next floating-point number from input */
+int getfloat(float *pn)
+{
+	int c, sign;
+	float power;
+
+	while (isspace(c = getch()))	/* skip white space */
+		;
+
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.')
+	{
+		ungetch(c);		/* it's not a number */
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-')
+		c = getch();
+	for (*pn = 0.0; isdigit(c); c = getch())                                                                                                                                     
+		*pn = 10.0 * *pn + (c - '0'); /* integer part C中默认浮点数是double型，这里做了double到float默认转换 */
+	if( c == '.' )
+		c = getch();
+	for (power = 1.0; isdigit(c); c = getch())
+	{
+		*pn = 10.0 * *pn + (c - '0'); /* fractional part C中默认浮点数是double型，这里做了double到float默认转换 */
+		power *= 10.0;
+	}
+	*pn *= sign / power;					/* final number */
+	if (c != EOF)
+		ungetch(c);                                                                                      
+	return c;
+}   
