156be9f64b6bd940ed74b839d77d58959c9b265a
adolphyang22 <2388965896@qq.com>
2018-05-19 14:32:43 +0800

    Create test1.h

---
+++ 5.5ec/test1.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <memory.h>
+
+#include <string.h>
+
+void strncpy_test(char *, char *, int);
+void strncat_test(char *, char *, int);
+int strncmp_test(char *, char *, int);
+
