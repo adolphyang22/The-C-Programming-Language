11bb66ef30208eecd520316fe49484ca7b0a013a
adolphyang22 <2388965896@qq.com>
2018-05-19 15:34:52 +0800

    Create month_day.cpp

---
+++ 5.7ex/month_day.cpp
@@ -0,0 +1,15 @@
+#include "test1.h"
+
+/* month_day：将一年的第几天转换为月/日 */
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+	int i, leap;
+
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+	for (i = 1; yearday > daytab[leap][i]; i++)
+		yearday -= daytab[leap][i];
+
+	*pmonth = i;
+	*pday = yearday;
+}
