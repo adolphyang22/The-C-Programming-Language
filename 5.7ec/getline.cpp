7addc782ba830041964755bee0ecf6b7fee8a919
adolphyang22 <2388965896@qq.com>
2018-05-19 15:20:03 +0800

    Create getline.cpp

---
+++ 5.7ec/getline.cpp
@@ -0,0 +1,19 @@
+#include "test1.h"
+
+/* getline: 将一行文本读取到s中， 并返回其长度 */
+int getline1(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+
